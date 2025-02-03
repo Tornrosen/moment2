d3dec149c40fc509c41df944bd12311ec021a20f
Tornrosen <elec11anna@hotmail.com>
2025-02-03 14:57:11 +0100

    skapa funktioner för att hämta o skriva ut data

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,34 @@
 
 let courses = [];
 
+//starta funktion när fönstret öppnas
+
+window.onload = () => {
+    loadCourses();
+}
+//hämta data
+
+async function loadCourses() {
+    try {
+const response = await fetch("https://webbutveckling.miun.se/files/ramschema_ht24.json");
+if (!response.ok) {
+    throw new Error("Fel vid anslutning...");
+}
+courses = await response.json();
+displayCourses(courses);
+    } catch (error) {
+        console.error(error);
+    }
+
+}
+
+//skriva ut data
+
+function displayCourses(data) {
+console.log(data);
+const coursesEl = document.querySelector("#publishInfo");
+coursesEl.innerHTML="";
+courses.forEach(course=>{
+coursesEl.innerHTML += `<tr><td>${course.code}</td><td>${course.coursename}</td><td>${course.progression}</td></tr>`
+}) 
+}
